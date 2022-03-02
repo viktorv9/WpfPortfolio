56d93a2b515ed06f64ab2bc91b40d60f940a209c
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2022-03-02 18:51:19 +0100

    start efcore to minimum api tutorial

--- PortfolioApi/Image.cs
+++ PortfolioApi/Image.cs
@@ -2,9 +2,17 @@ namespace PortfolioApi;
 
 public class Image
 {
+    public int Id { get; set; }
+
     public string Title { get; set; }
 
     public string LinkURL { get; set; }
 
     public string[] Tags { get; set; }
+    
+    class ImageDb : DbContext
+    {
+        public ImageDb(DbContextOptions options) : base(options) { }
+        public DbSet<Image> Images { get; set; }
+    }
 }
--- PortfolioApi/Program.cs
+++ PortfolioApi/Program.cs
@@ -1,25 +1,23 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo {
+         Title = "Images API",
+         Description = "Saving and requesting image data",
+         Version = "v1" });
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Images API V1");
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
